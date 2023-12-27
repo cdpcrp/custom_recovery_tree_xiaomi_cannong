e946122fb02445f771300e432399e895c45b34e2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-12-27 02:14:42 +0100

    cannong: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: M2007J22G Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_cannong.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_cannong-user \
+    omni_cannong-userdebug \
+    omni_cannong-eng
