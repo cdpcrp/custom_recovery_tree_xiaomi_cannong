01cf6c0f8a46463157983e1afa44d5d23160c1c2
Carlo Dee <carlodee.official@proton.me>
2024-01-23 11:39:32 +0800

    cannong: Update recovery script -- cleanups.
    * Removed the extra elif before the second condition. Each if statement should be independent.
    * Removed the unnecessary semicolon, commands are already separated per line anyway.
    * Defined variables for /tmp/vendor and /tmp/system to avoid repetitive path typing.
    
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- recovery/root/system/bin/postrecoveryboot.sh
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -1,30 +1,38 @@
 #!/system/bin/sh
 
-#
-# TODO: This postrecoveryboot shell script is needed for devices that has recovery-from-boot.p file in /vendor instead of /system, to prevent the stock ROM from replacing TWRP.
-# By default, TWRP has automatic feature for this, only if recovery-from-boot.p is in /system.
-# 
+# TODO: This postrecoveryboot shell script is needed for devices that have recovery-from-boot.p file in /vendor instead of /system, to prevent the stock ROM from replacing TWRP.
+# By default, TWRP has an automatic feature for this, only if recovery-from-boot.p is in /system.
 
-mkdir -p "/tmp/vendor";
-mkdir -p "/tmp/system";
-mount -w "/dev/block/mapper/vendor" "/tmp/vendor";
-mount -w "/dev/block/mapper/system" "/tmp/system";
+TMP_VENDOR="/tmp/vendor"
+TMP_SYSTEM="/tmp/system"
+
+mkdir -p "$TMP_VENDOR"
+mkdir -p "$TMP_SYSTEM"
+mount -w "/dev/block/mapper/vendor" "$TMP_VENDOR"
+mount -w "/dev/block/mapper/system" "$TMP_SYSTEM"
 
 # Android 12+
-if [ -f "/tmp/vendor/recovery-from-boot.p" ]; then
-  echo "I:postrecoveryboot: Removing stock recovery file in /vendor to prevent the stock ROM from replacing TWRP." >> /tmp/recovery.log;
-  rm "/tmp/vendor/bin/install-recovery.sh";
-  rm "/tmp/vendor/etc/init/vendor_flash_recovery.rc";
-  rm "/tmp/vendor/recovery-from-boot.p";
-elif [ -f "/tmp/system/system/recovery-from-boot.p" ]; then
-  # Android 11
-  rm "/tmp/system/system/recovery-from-boot.p" ];
-  rm "/tmp/system/system/bin/install-recovery.sh" ];
-fi;
+if [ -f "$TMP_VENDOR/recovery-from-boot.p" ]; then
+  echo "I:postrecoveryboot: Removing stock recovery file in /vendor to prevent the stock ROM from replacing TWRP." >> /tmp/recovery.log
+  rm "$TMP_VENDOR/bin/install-recovery.sh"
+  rm "$TMP_VENDOR/etc/init/vendor_flash_recovery.rc"
+  rm "$TMP_VENDOR/recovery-from-boot.p"
+else
+  echo "I:postrecoveryboot: Nothing to remove, /vendor/recovery-from-boot.p doesn't exist." >> /tmp/recovery.log
+fi
 
-umount "/tmp/vendor";
-umount "/tmp/system";
-rm -r "/tmp/vendor";
-rm -r "/tmp/system";
+# Android 11
+if [ -f "$TMP_SYSTEM/system/recovery-from-boot.p" ]; then
+  echo "I:postrecoveryboot: Removing stock recovery file in /system to prevent the stock ROM from replacing TWRP." >> /tmp/recovery.log
+  rm "$TMP_SYSTEM/system/recovery-from-boot.p"
+  rm "$TMP_SYSTEM/system/bin/install-recovery.sh"
+else
+  echo "I:postrecoveryboot: Nothing to remove, /system/recovery-from-boot.p doesn't exist." >> /tmp/recovery.log
+fi
 
-exit 0;
+umount "$TMP_VENDOR"
+umount "$TMP_SYSTEM"
+rm -r "$TMP_VENDOR"
+rm -r "$TMP_SYSTEM"
+
+exit 0
