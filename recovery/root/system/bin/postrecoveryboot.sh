60e333244f7d2ffa6581425eb38b265d1154efd7
Carlo Dee <carlodee.official@proton.me>
2024-01-25 11:46:25 +0800

    cannong: Update recovery script -- major cleanups.
    * Since we are not building TWRP for Android 11 and below, might as well removed it from the script.
    * Made some additional command for setting partition as read/mount -- mostly for devices that using read-only file-system (experimental).
    * Removed /system as part of the command since Android 12+ only uses /vendor now for stock recovery replace file.
    
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- recovery/root/system/bin/postrecoveryboot.sh
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -3,36 +3,48 @@
 # TODO: This postrecoveryboot shell script is needed for devices that have recovery-from-boot.p file in /vendor instead of /system, to prevent the stock ROM from replacing TWRP.
 # By default, TWRP has an automatic feature for this, only if recovery-from-boot.p is in /system.
 
-TMP_VENDOR="/tmp/vendor"
-TMP_SYSTEM="/tmp/system"
-
-mkdir -p "$TMP_VENDOR"
-mkdir -p "$TMP_SYSTEM"
-mount -w "/dev/block/mapper/vendor" "$TMP_VENDOR"
-mount -w "/dev/block/mapper/system" "$TMP_SYSTEM"
-
 # Android 12+
-if [ -f "$TMP_VENDOR/recovery-from-boot.p" ]; then
-  echo "I:postrecoveryboot: Removing stock recovery file in /vendor to prevent the stock ROM from replacing TWRP." >> /tmp/recovery.log
-  rm "$TMP_VENDOR/bin/install-recovery.sh"
-  rm "$TMP_VENDOR/etc/init/vendor_flash_recovery.rc"
-  rm "$TMP_VENDOR/recovery-from-boot.p"
-else
-  echo "I:postrecoveryboot: Nothing to remove, /vendor/recovery-from-boot.p doesn't exist." >> /tmp/recovery.log
-fi
 
-# Android 11
-if [ -f "$TMP_SYSTEM/system/recovery-from-boot.p" ]; then
-  echo "I:postrecoveryboot: Removing stock recovery file in /system to prevent the stock ROM from replacing TWRP." >> /tmp/recovery.log
-  rm "$TMP_SYSTEM/system/recovery-from-boot.p"
-  rm "$TMP_SYSTEM/system/bin/install-recovery.sh"
+# Target Partition
+TARGET_PARTITION="vendor"
+
+# Define Temporary Directory
+TMP_VENDOR="/tmp/$TARGET_PARTITION"
+
+# Create Temporary Directory
+mkdir -p "$TMP_VENDOR"
+
+# Function for setting and mounting as read/write
+set_mount_as_read_write() {
+  echo "I:postrecoveryboot: Setting $TARGET_PARTITION to read/write..." >> /tmp/recovery.log
+  blockdev --setrw "/dev/block/mapper/$TARGET_PARTITION"
+  sleep 1
+  echo "I:postrecoveryboot: Mounting $TARGET_PARTITION as read/write to $TMP_VENDOR..." >> /tmp/recovery.log
+  mount -o -rw "/dev/block/mapper/$TARGET_PARTITION" "$TMP_VENDOR"
+  sleep 1
+}
+
+# Function to remove stock recovery files
+remove_stock_recovery_files() {
+  echo "I:postrecoveryboot: Removing stock recovery files in /vendor to prevent the stock ROM from replacing TWRP..." >> /tmp/recovery.log
+  sleep 1
+  rm "$TMP_VENDOR/bin/install-recovery.sh" \
+     "$TMP_VENDOR/etc/init/vendor_flash_recovery.rc" \
+     "$TMP_VENDOR/recovery-from-boot.p"
+  sleep 1
+}
+
+# Run set_mount_as_read_write()
+set_mount_as_read_write
+
+# Run the function
+if [ -f "$TMP_VENDOR/recovery-from-boot.p" ]; then
+  remove_stock_recovery_files
 else
-  echo "I:postrecoveryboot: Nothing to remove, /system/recovery-from-boot.p doesn't exist." >> /tmp/recovery.log
+  echo "I:postrecoveryboot: Skipping the process, $TMP_VENDOR/recovery-from-boot.p doesn't exist." >> /tmp/recovery.log
 fi
 
 umount "$TMP_VENDOR"
-umount "$TMP_SYSTEM"
 rm -r "$TMP_VENDOR"
-rm -r "$TMP_SYSTEM"
 
 exit 0
