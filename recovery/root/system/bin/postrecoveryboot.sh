d823fa0e772e1be8c368f6c25b7cfc6f8565cb63
Carlo Dee <carlodee.official@proton.me>
2024-01-29 09:42:11 +0800

    cannong: Minor script cleanup.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- recovery/root/system/bin/postrecoveryboot.sh
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -2,6 +2,7 @@
 
 # TODO: This postrecoveryboot shell script is needed for devices that have recovery-from-boot.p file in /vendor instead of /system, to prevent the stock ROM from replacing TWRP.
 # By default, TWRP has an automatic feature for this, only if recovery-from-boot.p is in /system.
+# For EROFS devices, it might not work.
 
 # Android 12+
 
@@ -14,14 +15,10 @@ TMP_VENDOR="/tmp/$TARGET_PARTITION"
 # Create Temporary Directory
 mkdir -p "$TMP_VENDOR"
 
-# Function for setting and mounting as read/write
-set_mount_as_read_write() {
-  echo "I:postrecoveryboot: Setting $TARGET_PARTITION to read/write..." >> /tmp/recovery.log
-  blockdev --setrw "/dev/block/mapper/$TARGET_PARTITION"
-  sleep 1
-  echo "I:postrecoveryboot: Mounting $TARGET_PARTITION as read/write to $TMP_VENDOR..." >> /tmp/recovery.log
-  mount -o -rw "/dev/block/mapper/$TARGET_PARTITION" "$TMP_VENDOR"
-  sleep 1
+# Function for mounting with write permission
+mount_with_write_perm() {
+  echo "I:postrecoveryboot: Mounting $TARGET_PARTITION with write permission to $TMP_VENDOR..." >> /tmp/recovery.log
+  mount -w "/dev/block/mapper/$TARGET_PARTITION" "$TMP_VENDOR"
 }
 
 # Function to remove stock recovery files
@@ -31,11 +28,10 @@ remove_stock_recovery_files() {
   rm "$TMP_VENDOR/bin/install-recovery.sh" \
      "$TMP_VENDOR/etc/init/vendor_flash_recovery.rc" \
      "$TMP_VENDOR/recovery-from-boot.p"
-  sleep 1
 }
 
-# Run set_mount_as_read_write()
-set_mount_as_read_write
+# Run mount_with_write_perm()
+mount_with_write_perm
 
 # Run the function
 if [ -f "$TMP_VENDOR/recovery-from-boot.p" ]; then
