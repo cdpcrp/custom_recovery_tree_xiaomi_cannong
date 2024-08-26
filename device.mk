21f84a6278637649d52ba7c23fe26d41da7cbcb8
Carlo Dandan <carlodandan.personal@proton.me>
2024-08-26 06:36:10 +0000

    cannong: Rework decryption inits.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- device.mk
+++ device.mk
@@ -9,7 +9,7 @@ LOCAL_PATH := device/xiaomi/cannong
 
 # API/VNDK
 PRODUCT_SHIPPING_API_LEVEL := 29
-PRODUCT_TARGET_VNDK_VERSION := 32
+PRODUCT_TARGET_VNDK_VERSION := 30
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -21,12 +21,6 @@ PRODUCT_PACKAGES += \
 
 # Recovery additional binaries
 TARGET_RECOVERY_DEVICE_MODULES += \
-    android.hidl.allocator@1.0 \
-    android.hidl.memory@1.0 \
-    android.hidl.memory.token@1.0 \
-    libdmabufheap \
-    libhidlmemory \
-    libnetutils \
     libkeymaster4 \
     libkeymaster41 \
     libpuresoftkeymasterdevice \
@@ -34,12 +28,6 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     libxml2
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.allocator@1.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.memory@1.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.memory.token@1.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libhidlmemory.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libnetutils.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
