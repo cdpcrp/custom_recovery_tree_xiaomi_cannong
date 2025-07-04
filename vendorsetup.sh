6321cdcd66a9d07af61907a3df9040a25c09af7b
Carlo Dandan <carlodandan.personal@proton.me>
2025-07-04 02:37:47 +0000

    cannong: Record some errors in mounting logical partitions as log entries only.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -70,6 +70,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export OF_NO_SPLASH_CHANGE=1
     export OF_DEFAULT_KEYMASTER_VERSION=4.1
 
+    # Logical Partitions
+    export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
+
 # let's see what are our build VARs
     if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
         export | grep "FOX" >> $FOX_BUILD_LOG_FILE
